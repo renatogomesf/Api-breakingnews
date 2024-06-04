ca75b451f18246754fb62b3e193917dd07a0dad0
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2024-06-04 16:54:15 -0300

    Update user.service.js

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,10 +1,12 @@
 import User from "../models/User.js";
 
-// "create":  método do mongoose que cria um novo item dentro do schema e com seus padrões
+// "find":  método do mongoose que busca todos os itens dentro do schema e com seus padrões
 const getAll = () => User.find()
 
+// "findById":  método do mongoose que busca um item pelo id dentro do schema e com seus padrões
 const getId = (id) => User.findById(id)
 
+// "create":  método do mongoose que cria um novo item dentro do schema e com seus padrões
 const create = (body) => User.create(body)
 
 export default {
