9067c5483b59e8f99743337244799b96d46c69a9
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2024-06-09 11:23:11 -0300

    feita aula 12

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,64 +5,85 @@ class userController{
     
     async getAll(request, response){
 
-        const users = await userService.getAll()
-
-        if(users.length === 0){
-            return response.status(400).send({message: "There are no registered users"})
+        try{
+            const users = await userService.getAll()
+    
+            if(users.length === 0){
+                return response.status(400).send({message: "There are no registered users"})
+            }
+    
+            response.status(200).send(users)
         }
-
-        response.status(200).send(users)
+        catch(error){
+            response.status(500).send({error:"Connection error"})
+        }
     }
 
 
     async getId(request, response){
 
-        const users = request.user
-
-        response.status(200).send(users)
+        try{
+            const users = request.user
+    
+            response.status(200).send(users)
+        }
+        catch(error){
+            response.status(500).send({error:"Connection error"})
+        }
     }
 
 
     async post(request, response){
-        const {name, username, email, password, avatar, background} =request.body
 
-        if(!name || !username || !email || !password || !avatar || !background){
-            response.status(400).send({message:"Submit all fields for registration"})
-        }
-
-        const user = await userService.post(request.body)
-
-        if(!user){
-            return response.status(400).send({message: "Error creating user"})
-        }
-
-        response.status(201).send({
-            message: "User created successfully",
-            user:{
-                id: user._id,
-                name,
-                username,
-                email,
-                avatar,
-                background
+        try{
+            const {name, username, email, password, avatar, background} =request.body
+    
+            if(!name || !username || !email || !password || !avatar || !background){
+                response.status(400).send({message:"Submit all fields for registration"})
             }
-        })
+    
+            const user = await userService.post(request.body)
+    
+            if(!user){
+                return response.status(400).send({message: "Error creating user"})
+            }
+    
+            response.status(201).send({
+                message: "User created successfully",
+                user:{
+                    id: user._id,
+                    name,
+                    username,
+                    email,
+                    avatar,
+                    background
+                }
+            })
+        }
+        catch(error){
+            response.status(500).send({error})
+        }
     }
 
 
     async patch(request, response){
 
-        const {name, username, email, password, avatar, background} =request.body
-
-        if(!name && !username && !email && !password && !avatar && !background){
-            response.status(400).send({message:"Submit at least one field for update"})
+        try{
+            const {name, username, email, password, avatar, background} =request.body
+    
+            if(!name && !username && !email && !password && !avatar && !background){
+                response.status(400).send({message:"Submit at least one field for update"})
+            }
+    
+            const id = request.id
+    
+            await userService.patch(id,name,username,email,avatar,background)
+    
+            response.status(200).send({message: "User successfully updated"})
         }
-
-        const id = request.id
-
-        await userService.patch(id,name,username,email,avatar,background)
-
-        response.status(200).send({message: "User successfully updated"})
+        catch(error){
+            response.status(500).send({error:"Connection error"})
+        }
     }
 }
 
--- src/middlewares/global.middlewares.js
+++ src/middlewares/global.middlewares.js
@@ -2,28 +2,40 @@ import userService from "../services/user.service.js"
 import mongoose from "mongoose"
 
 const validId = (request, response, next)=>{
-    const id = request.params.id
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return response.status(400).send({message: "Invalid ID"})
+    try{
+        const id = request.params.id
+    
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return response.status(400).send({message: "Invalid ID"})
+        }
+    
+        next()
     }
-
-    next()
+    catch(error){
+        response.status(500).send({error:"Connection error"})
+    }
 }
 
 const validUser = async (request, response, next)=>{
-    const id = request.params.id
 
-    const users = await userService.getId(id)
-
-    if(!users){
-        return response.status(400).send({message: "User not found"})
+    try{
+        const id = request.params.id
+    
+        const users = await userService.getId(id)
+    
+        if(!users){
+            return response.status(400).send({message: "User not found"})
+        }
+    
+        request.id = id
+        request.user = users
+    
+        next()
     }
-
-    request.id = id
-    request.user = users
-
-    next()
+    catch(error){
+        response.status(500).send({error:"Connection error"})
+    }
 }
 
 export {validId, validUser}
